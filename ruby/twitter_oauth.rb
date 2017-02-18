79b235d34a9dc02085c65d2b0875ac537d40a397
Levi D. Smith <me@levidsmith.com>
2017-02-17 21:53:55 -0500

    Added different options for adding goodlist.txt by username or ID (default) makeGoodList now generates goodlist.txt file with IDs

--- ruby/twitter_oauth.rb
+++ ruby/twitter_oauth.rb
@@ -9,6 +9,10 @@ $oauth_token_secret = ""
 
 $access_token = nil
 
+SLEEP_MIN = 60
+SLEEP_MAX = 90
+
+
 def readTokens()
   f = File.new("tokens.txt", "r")
   $consumer_key = f.gets().chomp()
@@ -77,7 +81,7 @@ def followGoodList()
 
     response = $access_token.request(:post, "https://api.twitter.com/1.1/friendships/create.json?screen_name=#{username}&follow=true")
     puts response
-    iSleep = 30 + rand(30)
+    iSleep = SLEEP_MIN + rand(SLEEP_MAX - SLEEP_MIN)
     puts "Sleeping for #{iSleep} seconds"
     sleep(iSleep)
   }
@@ -87,20 +91,70 @@ def followGoodList()
   FileUtils.mv('goodlist.txt', strArchiveFile) 
 end
 
-def makeGoodList()
+def makeGoodList(username)
+
+  puts "Making goodlist.txt for #{username}"
+
+  # use the access token as an agent to get the home timeline
+  response = $access_token.request(:get, "https://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=#{username}&count=200")
+
+  puts response
+  #puts response["followers_count"]
+
   f = File.new("goodlist.txt", "w")
-  f.puts("Hello")
-  f.puts("World")
+
+#  puts "RESULT: " + JSON.parse(response.body)
+  users = JSON.parse(response.body)
+#  puts users["next_cursor"]
+  puts users["ids"]
+  users["ids"].each do |id|
+    puts "ID: #{id}"
+    f.puts "#{id}"
+  end
+
+#  users.map do | user |
+#       puts "#{i}: " + user["id"]
+#      puts " " + "User: " + user["screen_name"]
+#    puts user
+#    puts user['ids']
+#  end
+
   f.close()
 
 end
+
+def followGoodListByID()
+  arrayGoodList = Array.new()
+  File.open("goodlist.txt").each do |line|
+    arrayGoodList << line.chomp()
+  end
+
+  arrayGoodList.each { |userid|
+
+    puts "Follwing #{userid}"
+
+    response = $access_token.request(:post, "https://api.twitter.com/1.1/friendships/create.json?user_id=#{userid}&follow=true")
+    puts response
+    iSleep = SLEEP_MIN + rand(SLEEP_MAX - SLEEP_MIN)
+    puts "Sleeping for #{iSleep} seconds"
+    sleep(iSleep)
+  }
+
+  strArchiveFile = "goodlist" + Time.now.strftime("%Y%m%d_%H%M%S") + ".txt"
+  puts "Moving goodlist.txt to #{strArchiveFile}"
+  FileUtils.mv('goodlist.txt', strArchiveFile) 
+end
+
+
 
 def displayUsage() 
     puts "Usage: ruby twitter_oauth.rb <command> <options>"
     puts "Commands: hometweets - list tweets in your timeline"
     puts "          follow <username> - follows the specified user"
-    puts "          followgoodlist  - follows everyone in the goodlist.txt file"
-    puts "          makegoodlist <username>  - creates goodlist.txt file using followers of the specified user"
+    puts "          followgoodlist    - follows all IDs in goodlist.txt file"
+    puts "          followgoodlistusername  - follows all handles in goodlist.txt file"
+    puts "          makegoodlist <username>  - creates goodlist.txt file using "
+    puts "            followers of the specified user"
 
 end
 
@@ -118,9 +172,11 @@ def main()
   elsif (ARGV[0].upcase == "FOLLOW") 
     followUser(ARGV[1]) 
   elsif (ARGV[0].upcase == "FOLLOWGOODLIST") 
-    followGoodList()
+      followGoodListByID()
+  elsif (ARGV[0].upcase == "FOLLOWGOODLISTUSERNAME") 
+      followGoodList()
   elsif (ARGV[0].upcase == "MAKEGOODLIST") 
-    makeGoodList()
+    makeGoodList(ARGV[1])
   else 
     puts "Invalid option"
     displayUsage()
